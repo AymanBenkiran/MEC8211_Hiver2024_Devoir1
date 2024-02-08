331b80365d0ffddb93e6c906b6155c1c2eb6a6c8
Amishga Alphonius <amishga1999@gmail.com>
2024-02-08 17:01:05 -0500

    Rework structure of code

--- code_amishga/devoir1_functions.py
+++ code_amishga/devoir1_functions.py
@@ -18,17 +18,17 @@ temps et en espace).
     - En r = 0 : Un schema Gear avant est utilise pour approximer le gradient 
                  de concentration (ordre 2)
     - Pour les points centraux : 
-        - Derivee premeire : differentiation avant (ordre 1)
+        - Derivee premiere : differentiation avant (ordre 1)
         - Derivee seconde : differentiation centree (ordre 2)
-    - En r = R : Une condition de Dirichlet est imposee
+    - En r = r : Une condition de Dirichlet est imposee
 
 Entrees : 
     - prm_prob : Objet qui contient les parametres du probleme
-        - C0 : Concentrations initiales [mol/m^3]
-        - Ce : Concentration de sel de l'eau salee [mol/m^3]
-        - Deff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-        - R : Rayon du pilier cylindrique [m^3]
-        - S : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+        - c0 : Concentrations initiales [mol/m^3]
+        - ce : Concentration de sel de l'eau salee [mol/m^3]
+        - d_eff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+        - r : Rayon du pilier cylindrique [m^3]
+        - s : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
     - prm_sim : Objet qui contient les parametres de simulation
         
 Sorties : 
@@ -43,8 +43,8 @@ def mdf1_rxn_0(prm_prob, prm_sim):
     B = np.zeros(n)
 
     # Condition initiale
-    c = np.full(n, prm_prob.C0)
-    c[-1] = prm_prob.Ce
+    c = np.full(n, prm_prob.c0)
+    c[-1] = prm_prob.ce
 
     while diff > prm_sim.tol:
         sum_c_prec = sum(c)
@@ -56,19 +56,19 @@ def mdf1_rxn_0(prm_prob, prm_sim):
         B[0] = 0
 
         # Points centraux
-        cst1 = prm_sim.dt*prm_prob.Deff
+        cst1 = prm_sim.dt*prm_prob.d_eff
         for i in range(1, n-1):
             cst2 = prm_sim.dr**2 * prm_sim.mesh[i]  # r_i * dr^2
             A[i][i-1] = -cst1*prm_sim.mesh[i]
             A[i][i] = (cst2 + cst1*(prm_sim.dr + 2*prm_sim.mesh[i]))
             A[i][i+1] = -cst1*(prm_sim.dr + prm_sim.mesh[i])
-            B[i] = cst2*(c[i] - prm_sim.dt*prm_prob.S)
+            B[i] = cst2*(c[i] - prm_sim.dt*prm_prob.s)
 
-        # Dirichlet en r = R
+        # Dirichlet en r = r
         A[-1][-1] = 1
-        B[-1] = prm_prob.Ce
+        B[-1] = prm_prob.ce
 
-        # Resolution du systeme lineraire
+        # Resolution du systeme lineaire
         c = np.linalg.solve(A, B)
         tf += prm_sim.dt
         diff = abs(sum(c)-sum_c_prec)/abs(sum_c_prec)
@@ -85,15 +85,15 @@ temps et 2 en espace).
     - Pour les points centraux : 
         - Derivee premiere : differentiation centree (ordre 2)
         - Derivee seconde : differentiation centree (ordre 3)
-    - En r = R : Une condition de Dirichlet est imposee
+    - En r = r : Une condition de Dirichlet est imposee
 
 Entrees : 
     - prm_prob : Objet qui contient les parametres du probleme
-        - C0 : Concentrations initiales [mol/m^3]
-        - Ce : Concentration de sel de l'eau salee [mol/m^3]
-        - Deff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-        - R : Rayon du pilier cylindrique [m^3]
-        - S : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+        - c0 : Concentrations initiales [mol/m^3]
+        - ce : Concentration de sel de l'eau salee [mol/m^3]
+        - d_eff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+        - r : Rayon du pilier cylindrique [m^3]
+        - s : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
     - prm_sim : Objet qui contient les parametres de simulation
         
 Sorties : 
@@ -102,14 +102,14 @@ Sorties :
 """
 def mdf2_rxn_0(prm_prob, prm_sim):
     tf = 0
-    diff = 1
+    diff = prm_sim.tol+1
     n = prm_sim.n_noeuds
     A = np.zeros((n, n))
     B = np.zeros(n)
 
     # Condition initiale
-    c = np.full(n, prm_prob.C0)
-    c[-1] = prm_prob.Ce
+    c = np.full(n, prm_prob.c0)
+    c[-1] = prm_prob.ce
 
     while diff > prm_sim.tol:
         sum_c_prec = sum(c)
@@ -121,19 +121,19 @@ def mdf2_rxn_0(prm_prob, prm_sim):
         B[0] = 0
 
         # Points centraux
-        cst1 = prm_sim.dt*prm_prob.Deff
+        cst1 = prm_sim.dt*prm_prob.d_eff
         for i in range(1, n-1):
             cst2 = 2 * prm_sim.dr**2 * prm_sim.mesh[i]  # 2 * r_i * dr^2
             A[i][i-1] = cst1*(prm_sim.dr - 2*prm_sim.mesh[i])
             A[i][i] = cst2 + 4*cst1*prm_sim.mesh[i]
             A[i][i+1] = -cst1*(prm_sim.dr + 2*prm_sim.mesh[i])
-            B[i] = cst2*(c[i] - prm_sim.dt*prm_prob.S)
+            B[i] = cst2*(c[i] - prm_sim.dt*prm_prob.s)
 
-        # Dirichlet en r = R
+        # Dirichlet en r = r
         A[-1][-1] = 1
-        B[-1] = prm_prob.Ce
+        B[-1] = prm_prob.ce
 
-        # Resolution du systeme lineraire
+        # Resolution du systeme lineaire
         c = np.linalg.solve(A, B)
         tf += prm_sim.dt
         diff = abs(sum(c)-sum_c_prec)/abs(sum_c_prec)
@@ -146,18 +146,18 @@ Fonction qui calcule la solution analytique aux points du maillage.
 
 Entrees :
     - prm_prob : Objet qui contient les parametres du probleme
-        - C0 : Concentrations initiales [mol/m^3]
-        - Ce : Concentration de sel de l'eau salee [mol/m^3]
-        - Deff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-        - R : Rayon du pilier cylindrique [m^3]
-        - S : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+        - c0 : Concentrations initiales [mol/m^3]
+        - ce : Concentration de sel de l'eau salee [mol/m^3]
+        - d_eff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+        - r : Rayon du pilier cylindrique [m^3]
+        - s : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
     - mesh : Vecteur conteant les noeuds (r_i) du probleme 1D [m]
     
 Sortie :
     - c : Le profile de concentration radial analytique au régime permanent [mol/m^3]
 """
 def analytique(prm_prob, mesh):
-    c = [0.25*prm_prob.S/prm_prob.Deff * prm_prob.R**2 * (r**2/prm_prob.R**2 - 1) + prm_prob.Ce for r in mesh]
+    c = [0.25*prm_prob.s/prm_prob.d_eff * prm_prob.r**2 * (r**2/prm_prob.r**2 - 1) + prm_prob.ce for r in mesh]
     return c
 
 
@@ -166,8 +166,8 @@ def analytique(prm_prob, mesh):
 Fonction qui calcule la valeur de l'erreur L1 de la solution numerique obtenue
 
 Entree :
-    - c_num = Solution numerique du probleme 1D [mol/m^3]
-    - c_analytique = Solution analytique du probleme 1D [mol/m^3]
+    - c_num : Solution numerique du probleme 1D [mol/m^3]
+    - c_analytique : Solution analytique du probleme 1D [mol/m^3]
 
 Sortie :
     - erreur : Norme de l'erreur L1 de la solution numerique [mol/m^3]
@@ -183,8 +183,8 @@ def erreur_l1(c_num, c_analytique):
 Fonction qui calcule la valeur de l'erreur L2 de la solution numerique obtenue
 
 Entree :
-    - c_num = Solution numerique du probleme 1D [mol/m^3]
-    - c_analytique = Solution analytique du probleme 1D [mol/m^3]
+    - c_num : Solution numerique du probleme 1D [mol/m^3]
+    - c_analytique : Solution analytique du probleme 1D [mol/m^3]
 
 Sortie :
     - erreur : Norme de l'erreur L2 de la solution numerique [mol/m^3]
@@ -201,8 +201,8 @@ def erreur_l2(c_num, c_analytique):
 Fonction qui calcule la valeur de l'erreur L_\infty de la solution numerique obtenue
 
 Entree :
-    - c_num = Solution numerique du probleme 1D [mol/m^3]
-    - c_analytique = Solution analytique du probleme 1D [mol/m^3]
+    - c_num : Solution numerique du probleme 1D [mol/m^3]
+    - c_analytique : Solution analytique du probleme 1D [mol/m^3]
 
 Sortie :
     - erreur : Norme de l'erreur L_\infty de la solution numerique [mol/m^3]
--- code_amishga/devoir1_main.py
+++ code_amishga/devoir1_main.py
@@ -20,22 +20,34 @@ except:
     print("ERREUR ! Il y a une erreur fatale dans le fichier devoir1_functions.py")
 
 #%% Donnees du probleme et definition des classes
+
 """
-Class qui contient les differents parametres du probleme selon le type de terme source
+Classe qui contient les differents parametres du probleme selon le type de terme source
 
 Entree:
-    - ordre_de_rxn : Ordre de la reaction (0 ou 1)
+    - ordre_de_rxn : Ordre de la reaction (0 ou 1) []
+    
+Attributs:
+    - c0 : float - Concentrations initiales [mol/m^3]
+    - ce : float - Concentration de sel de l'eau salee [mol/m^3]
+    - r : float - Rayon du pilier cylindrique [m]
+    - d_eff : float - Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+    - ordre_de_rxn : int - Ordre de la reaction (0 ou 1) []
+    - s : float - Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+    - k : float - Constante de réaction pour la reaction d'ordre 1 [s^{-1}]
 """
 class ParametresProb:
     def __init__(self, ordre_de_rxn=0):
-        self.C0 = 0         # Concentrations initiales [mol/m^3]
-        self.Ce = 12        # Concentration de sel de l'eau salee [mol/m^3]
-        self.R = 1*0.5      # Rayon du pilier cylindrique [m^3]
-        self.Deff = 1e-10   # Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-        if ordre_de_rxn == 0:
-            self.S = 8e-9   # Terme source constant (reaction d'ordre 0) [mol/m^3/s]
-        elif ordre_de_rxn == 1:
-            self.k = 4e-9   # Constante de réaction pour la reaction d'ordre 1 [s^{-1}]
+        self.c0 = 0.0
+        self.ce = 12.0
+        self.r = 1*0.5
+        self.d_eff = 1e-10
+        self.ordre_de_rxn = ordre_de_rxn
+        if self.ordre_de_rxn == 0:
+            self.s = 8e-9
+        elif self.ordre_de_rxn == 1:
+            self.k = 4e-9
+            raise Exception("Cet ordre n'est pas encore implememte.")
         else:
             raise Exception("L'ordre de reaction doit etre de 0 ou 1.")
 
@@ -44,32 +56,39 @@ Classe qui contient les parametres de simulation pour une simulation donnee
 
 Entrees:
     - prm_rxn : Object contenant les donnees du probleme
-        - R : Rayon du pilier cylindrique [m^3]
+        - r : Rayon du pilier cylindrique [m^3]
     - p_n_noeuds : Nombre de noeuds dans le maillage [noeud]
+    - p_mdf : 
    
 Attributs :
-    - n_noeuds : Nombre de noeuds dans le maillage [noeud]
-    - dr : Pas en espace des differents maillages [m]
-    - dt : Pas de temps des differents maillages [s]
-    - mesh : Vecteur conteant les noeuds (r_i) du probleme 1D [m]
-    - tol : Tolerance relative pour l'atteinte du regime permanent []
-    - c : 
+    - n_noeuds : int - Nombre de noeuds dans le maillage [noeud]
+    - dr : float - Pas en espace des differents maillages [m]
+    - dt : float - Pas de temps des differents maillages [s]
+    - mesh : array of floats - Vecteur conteant les noeuds (r_i) du probleme 1D [m]
+    - tol : float - Tolerance relative pour l'atteinte du regime permanent []
+    - c : array of floats - Solution une fois l'atteinte du regime permanent [mol/m^3]
+    - tf : float - Temps de fin de la simulation [s]
+    - mdf : 
+    - ordre_de_rxn : 
 """
 class ParametresSim:
-    def __init__(self, prm_rxn, p_n_noeuds):
-        self.n_noeuds = p_n_noeuds                          # Nombre de noeuds dans le maillage [noeud]
-        self.dr = prm_rxn.R/(self.n_noeuds-1)               # Pas en espace des differents maillages [m]
-        self.dt = 0.5*self.dr**2/prm_rxn.Deff               # Pas de temps des differents maillages [s]
-        self.mesh = np.linspace(0,prm_rxn.R,self.n_noeuds)  # Vecteur conteant les noeuds (r_i) du probleme 1D [m]
-        self.tol = 1e-8                                     # Tolerance relative pour l'atteinte du regime permanent []
-        self.c = np.zeros(self.n_noeuds)                    # Solution une fois l'atteinte du regime permanent [mol/m^3]
-        self.tf = 0                                         # Temps de fin de la simulation [s]
+    def __init__(self, prm_rxn, p_n_noeuds, p_mdf):
+        self.n_noeuds = p_n_noeuds
+        self.dr = prm_rxn.r/(self.n_noeuds-1)
+        self.dt = 0.5*self.dr**2/prm_rxn.d_eff
+        self.mesh = np.linspace(0,prm_rxn.r,self.n_noeuds)
+        self.tol = 1e-8
+        self.c = np.zeros(self.n_noeuds)
+        self.tf = 0
+        self.mdf = p_mdf
+        self.ordre_de_rxn = 0
 
 
-#%% Initialisation des objects contenants les parametres du probleme pour une reaction d'ordre 0 et 1
+#%% Initialisation des objects contenants les parametres du probleme pour une reaction d'ordre 0
+#   et 1
 
 prm_rxn_0 = ParametresProb(ordre_de_rxn=0)
-prm_rxn_1 = ParametresProb(ordre_de_rxn=1)
+# prm_rxn_1 = ParametresProb(ordre_de_rxn=1) # pas encore implemente
 
 
 #%% Discretisation pour la reaction d'ordre 0
@@ -80,75 +99,68 @@ n_noeuds = [10, 20, 40, 80, 160] # Liste de nombre de noeuds pour les differents
 prm_simulations_mdf1_rxn0 = []
 prm_simulations_mdf2_rxn0 = []
 for i in range(len(n_noeuds)):
-    prm_simulations_mdf1_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds[i]))
-    prm_simulations_mdf2_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds[i]))
+    prm_simulations_mdf1_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds[i], 1))
+    prm_simulations_mdf2_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds[i], 2))
 
 
 #%% Resolution du probleme
 
-# for prm_sim in prm_simulations_mdf1_rxn0:
-#
-#     # Calcul de la concentration au regime permanent
-#     tf,prm_sim.c = mdf1_rxn_0(prm_rxn_0, prm_sim)
-#
-#     # Exportation des solutions dans des fichiers csv
-#     exported_data = pd.DataFrame({'r': prm_sim.mesh, 'C(r)': prm_sim.c})
-#     exported_data.to_csv(f"./solutions/mdf1_rxn0_noeuds_{str(prm_sim.n_noeuds).zfill(3)}.csv", index=False)
-#
-#     # Affichage au terminal
-#     print(f"****************************************************************************")
-#     print(f"Nombre de noeuds : {prm_sim.n_noeuds}")
-#     print(f"dr = {prm_sim.dr} m")
-#     print(f"dt = {prm_sim.dt} s")
-#     print(f"tf = {tf} s")
-#     print(f"****************************************************************************")
+for prm_simulation in [prm_simulations_mdf1_rxn0, prm_simulations_mdf2_rxn0]:
+    for prm_sim in prm_simulation:
+        mdf_i = prm_sim.mdf
+        # Calcul de la concentration au regime permanent
+        exec(f"prm_sim.tf,prm_sim.c = mdf{mdf_i}_rxn_0(prm_rxn_0, prm_sim)")
 
-for prm_sim in prm_simulations_mdf2_rxn0:
+        # Exportation des solutions dans des fichiers csv
+        exported_data = pd.DataFrame({'r': prm_sim.mesh, 'C(r)': prm_sim.c})
+        exported_data.to_csv(f"./solutions/mdf{mdf_i}_rxn0_noeuds_"
+                             f"{str(prm_sim.n_noeuds).zfill(3)}.csv", index=False)
 
-    # Calcul de la concentration au regime permanent
-    tf,prm_sim.c = mdf2_rxn_0(prm_rxn_0, prm_sim)
-
-    # Exportation des solutions dans des fichiers csv
-    exported_data = pd.DataFrame({'r': prm_sim.mesh, 'C(r)': prm_sim.c})
-    exported_data.to_csv(f"./solutions/mdf2_rxn0_noeuds_{str(prm_sim.n_noeuds).zfill(3)}.csv", index=False)
-
-    # Affichage au terminal
-    print(f"****************************************************************************")
-    print(f"Nombre de noeuds : {prm_sim.n_noeuds}")
-    print(f"dr = {prm_sim.dr} m")
-    print(f"dt = {prm_sim.dt} s")
-    print(f"tf = {tf} s")
-    print(f"****************************************************************************")
-
+        # Affichage au terminal
+        print("****************************************************************************")
+        print(f"Nombre de noeuds : {prm_sim.n_noeuds} noeuds")
+        print(f"dr = {prm_sim.dr} m")
+        print(f"dt = {prm_sim.dt} s")
+        print(f"tf = {prm_sim.tf} s")
+        print("****************************************************************************")
 
 #%% Etude de l'erreur
 
 # Initialisation de listes pour entreposer les donnees pour l'affichage graphique
-dr = []
-liste_erreur_l1 = []
-liste_erreur_l2 = []
-liste_erreur_linfty = []
+dr = []                     # Pas en espace [m]
+liste_erreur_l1 = []        # Erreur L1 [mol/m^3]
+liste_erreur_l2 = []        # Erreur L2
+liste_erreur_linfty = []    # Erreur Linfty
 
 # Calcul des erreurs pour les differentes simualtions
-for prm_sim in prm_simulations_mdf2_rxn0:
-    dr.append(prm_sim.dr)                                               # Pas en espace [m]
-    c_analytique = analytique(prm_rxn_0, prm_sim.mesh)                  # Solution analytique [mol/m^3]
-    liste_erreur_l1.append(erreur_l1(prm_sim.c, c_analytique))          # Erreur L1
-    liste_erreur_l2.append(erreur_l2(prm_sim.c, c_analytique))          # Erreur L2
-    liste_erreur_linfty.append(erreur_linfty(prm_sim.c, c_analytique))  # Erreur L_\infty
+for prm_simulation in [prm_simulations_mdf1_rxn0, prm_simulations_mdf2_rxn0]:
+    dr = []
+    liste_erreur_l1 = []
+    liste_erreur_l2 = []
+    liste_erreur_linfty = []
+    for prm_sim in prm_simulation:
+        mdf_i = prm_sim.mdf
+        dr.append(prm_sim.dr)
+        c_analytique = analytique(prm_rxn_0, prm_sim.mesh)  # Solution analytique [mol/m^3]
+        liste_erreur_l1.append(erreur_l1(prm_sim.c, c_analytique))
+        liste_erreur_l2.append(erreur_l2(prm_sim.c, c_analytique))
+        liste_erreur_linfty.append(erreur_linfty(prm_sim.c, c_analytique))
 
-# Exportation des valeurs d'erreur dans un fichier csv
-exported_data = pd.DataFrame({'dr': dr, 'L1_error': liste_erreur_l1, 'L2_error': liste_erreur_l2, 'Linfty_error': liste_erreur_linfty})
-exported_data.to_csv("./erreurs/mdf2_rxn0.csv", index=False)
+    # Exportation des valeurs d'erreur dans un fichier csv
+    exported_data = pd.DataFrame({'dr': dr, 'L1_error': liste_erreur_l1,
+                                  'L2_error': liste_erreur_l2,
+                                  'Linfty_error': liste_erreur_linfty})
+    exported_data.to_csv(f"./erreurs/mdf{mdf_i}_rxn0.csv", index=False)
 
-# Affichage graphique
-plt.loglog(dr,liste_erreur_l1, "s-", label=r"Erreur L$^1$")
-plt.loglog(dr,liste_erreur_l2, "s-", label=r"Erreur L$^2$")
-plt.loglog(dr,liste_erreur_linfty, "s-", label=r"Erreur L$^\infty$")
-plt.xlabel(r"$\Delta r$ [m]")
-plt.ylabel(r"Erreur [mol/m$^3$]")
-plt.title("")
-plt.legend()
-plt.grid()
-plt.savefig("erreurs_mdf2.png", dpi=600)
-plt.show()
+    # Affichage graphique
+    plt.figure()
+    plt.loglog(dr,liste_erreur_l1, "s-", label=r"Erreur L$_1$")
+    plt.loglog(dr,liste_erreur_l2, "s-", label=r"Erreur L$_2$")
+    plt.loglog(dr,liste_erreur_linfty, "s-", label=r"Erreur L$_\infty$")
+    plt.xlabel(r"$\Delta r$ [m]")
+    plt.ylabel(r"Erreur [mol/m$^3$]")
+    plt.title("")
+    plt.legend()
+    plt.grid()
+    plt.savefig(f"erreurs_mdf{mdf_i}.png", dpi=600)
+    plt.show()
