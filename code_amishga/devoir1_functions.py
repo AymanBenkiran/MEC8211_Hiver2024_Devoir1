cc62ca201a00bf0fac2d21a019f9d58f0de024a6
Amishga Alphonius <amishga1999@gmail.com>
2024-02-08 23:00:39 -0500

    Address most of pylint comments

--- code_amishga/devoir1_functions.py
+++ code_amishga/devoir1_functions.py
@@ -4,43 +4,52 @@ Fichier : devoir1_functions.py
 Description : Fichier secondaire contenant les fonctions pour le devoir 1
               (a utiliser conjointement avec devoir1_main.py)
 Auteur.es :
-Date de creation du fichier : 5 février 2024
+Date de creation du fichier : 5 féerier 2024
 """
 
 #%% Importation des modules
 import numpy as np
 
 #%% mdf1_rxn_0
-"""
-Fonction qui resout par le probleme transitoire jusqu'a l'atteinte du regime 
-permanent par la methode des differences finies (Schemas d'ordre globaux 1 en 
-temps et en espace).
-    - En r = 0 : Un schema Gear avant est utilise pour approximer le gradient 
-                 de concentration (ordre 2)
-    - Pour les points centraux : 
-        - Derivee premiere : differentiation avant (ordre 1)
-        - Derivee seconde : differentiation centree (ordre 2)
-    - En r = r : Une condition de Dirichlet est imposee
-
-Entrees : 
-    - prm_prob : Objet qui contient les parametres du probleme
-        - c0 : Concentrations initiales [mol/m^3]
-        - ce : Concentration de sel de l'eau salee [mol/m^3]
-        - d_eff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-        - r : Rayon du pilier cylindrique [m^3]
-        - s : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
-    - prm_sim : Objet qui contient les parametres de simulation
-        
-Sorties : 
-    - tf : Temps de fin de simulation [s]
-    - c : Vecteur des concentrations au regime permanent [mol/m^3]
-"""
 def mdf1_rxn_0(prm_prob, prm_sim):
+    """
+    Fonction qui resout par le probleme transitoire jusqu'a l'atteinte du regime
+    permanent par la methode des differences finies (Schemas d'ordre globaux 1 en
+    temps et en espace).
+        - En r = 0 : Un schema Gear avant est utilise pour approximer le gradient
+                     de concentration (ordre 2)
+        - Pour les points centraux :
+            - Derivee premiere : differentiation avant (ordre 1)
+            - Derivee seconde : differentiation centree (ordre 2)
+        - En r = R : Une condition de Dirichlet est imposee
+
+    Entrees :
+        - prm_prob : Objet qui contient les parametres du probleme
+            - c0 : float - Concentrations initiales [mol/m^3]
+            - ce : float - Concentration de sel de l'eau salee [mol/m^3]
+            - r : float - Rayon du pilier cylindrique [m]
+            - d_eff : float - Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+            - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source (0 ou 1) []
+            - s : float - Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+            - k : float - Constante de réaction pour la reaction d'ordre 1 [s^{-1}]
+        - prm_sim : Objet qui contient les parametres de simulation
+            - n_noeuds : int - Nombre de noeuds dans le maillage [noeud]
+            - dr : float - Pas en espace des differents maillages [m]
+            - dt : float - Pas de temps des differents maillages [s]
+            - mesh : array of floats - Vecteur conteant les noeuds (r_i) du probleme 1D [m]
+            - tol : float - Tolerance relative pour l'atteinte du regime permanent []
+            - c : array of floats - Solution une fois l'atteinte du regime permanent [mol/m^3]
+            - tf : float - Temps de fin de la simulation [s]
+            - mdf : int - Ordre global en espace de la methode des differences finies utilisee []
+            - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source []
+
+    Sortie : aucune
+    """
     tf = 0
     diff = 1
     n = prm_sim.n_noeuds
-    A = np.zeros((n, n))
-    B = np.zeros(n)
+    a = np.zeros((n, n))
+    b = np.zeros(n)
 
     # Condition initiale
     c = np.full(n, prm_prob.c0)
@@ -49,63 +58,66 @@ def mdf1_rxn_0(prm_prob, prm_sim):
     while diff > prm_sim.tol:
         sum_c_prec = sum(c)
 
-        # Gear avant en r = 0
-        A[0][0] = 3
-        A[0][1] = -4
-        A[0][2] = 1
-        B[0] = 0
+        # Conditions frontieres
+        appliquer_conditions_frontieres(a, b, prm_prob.ce)
 
         # Points centraux
         cst1 = prm_sim.dt*prm_prob.d_eff
         for i in range(1, n-1):
             cst2 = prm_sim.dr**2 * prm_sim.mesh[i]  # r_i * dr^2
-            A[i][i-1] = -cst1*prm_sim.mesh[i]
-            A[i][i] = (cst2 + cst1*(prm_sim.dr + 2*prm_sim.mesh[i]))
-            A[i][i+1] = -cst1*(prm_sim.dr + prm_sim.mesh[i])
-            B[i] = cst2*(c[i] - prm_sim.dt*prm_prob.s)
-
-        # Dirichlet en r = r
-        A[-1][-1] = 1
-        B[-1] = prm_prob.ce
+            a[i][i-1] = -cst1*prm_sim.mesh[i]
+            a[i][i] = cst2 + cst1*(prm_sim.dr + 2*prm_sim.mesh[i])
+            a[i][i+1] = -cst1*(prm_sim.dr + prm_sim.mesh[i])
+            b[i] = cst2*(c[i] - prm_sim.dt*prm_prob.s)
 
         # Resolution du systeme lineaire
-        c = np.linalg.solve(A, B)
+        c = np.linalg.solve(a, b)
         tf += prm_sim.dt
         diff = abs(sum(c)-sum_c_prec)/abs(sum_c_prec)
-    return tf, c
+    prm_sim.c = c
+    prm_sim.tf = tf
 
 
 #%% mdf2_rxn_0
-"""
-Fonction qui resout par le probleme transitoire jusqu'a l'atteinte du regime 
-permanent par la methode des differences finies (Schemas d'ordre globaux 1 en 
-temps et 2 en espace).
-    - En r = 0 : Un schema Gear avant est utilise pour approximer le gradient 
-                 de concentration (ordre 2)
-    - Pour les points centraux : 
-        - Derivee premiere : differentiation centree (ordre 2)
-        - Derivee seconde : differentiation centree (ordre 3)
-    - En r = r : Une condition de Dirichlet est imposee
-
-Entrees : 
-    - prm_prob : Objet qui contient les parametres du probleme
-        - c0 : Concentrations initiales [mol/m^3]
-        - ce : Concentration de sel de l'eau salee [mol/m^3]
-        - d_eff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-        - r : Rayon du pilier cylindrique [m^3]
-        - s : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
-    - prm_sim : Objet qui contient les parametres de simulation
-        
-Sorties : 
-    - tf : Temps de fin de simulation [s]
-    - c : Vecteur des concentrations au regime permanent [mol/m^3]
-"""
 def mdf2_rxn_0(prm_prob, prm_sim):
+    """
+    Fonction qui resout par le probleme transitoire jusqu'a l'atteinte du regime
+    permanent par la methode des differences finies (Schemas d'ordre globaux 1 en
+    temps et 2 en espace).
+        - En r = 0 : Un schema Gear avant est utilise pour approximer le gradient
+                     de concentration (ordre 2)
+        - Pour les points centraux :
+            - Derivee premiere : differentiation centree (ordre 2)
+            - Derivee seconde : differentiation centree (ordre 3)
+        - En r = R : Une condition de Dirichlet est imposee
+
+    Entrees :
+        - prm_prob : Objet qui contient les parametres du probleme
+            - c0 : float - Concentrations initiales [mol/m^3]
+            - ce : float - Concentration de sel de l'eau salee [mol/m^3]
+            - r : float - Rayon du pilier cylindrique [m]
+            - d_eff : float - Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+            - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source (0 ou 1) []
+            - s : float - Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+            - k : float - Constante de réaction pour la reaction d'ordre 1 [s^{-1}]
+        - prm_sim : Objet qui contient les parametres de simulation
+            - n_noeuds : int - Nombre de noeuds dans le maillage [noeud]
+            - dr : float - Pas en espace des differents maillages [m]
+            - dt : float - Pas de temps des differents maillages [s]
+            - mesh : array de floats - Vecteur conteant les noeuds (r_i) du probleme 1D [m]
+            - tol : float - Tolerance relative pour l'atteinte du regime permanent []
+            - c : array de float - Solution une fois l'atteinte du regime permanent [mol/m^3]
+            - tf : float - Temps de fin de la simulation [s]
+            - mdf : int - Ordre global en espace de la methode des differences finies utilisee []
+            - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source []
+
+    Sortie : aucune
+    """
     tf = 0
     diff = prm_sim.tol+1
     n = prm_sim.n_noeuds
-    A = np.zeros((n, n))
-    B = np.zeros(n)
+    a = np.zeros((n, n))
+    b = np.zeros(n)
 
     # Condition initiale
     c = np.full(n, prm_prob.c0)
@@ -114,99 +126,127 @@ def mdf2_rxn_0(prm_prob, prm_sim):
     while diff > prm_sim.tol:
         sum_c_prec = sum(c)
 
-        # Gear avant en r = 0
-        A[0][0] = 3
-        A[0][1] = -4
-        A[0][2] = 1
-        B[0] = 0
+        # Conditions frontieres
+        appliquer_conditions_frontieres(a, b, prm_prob.ce)
 
         # Points centraux
         cst1 = prm_sim.dt*prm_prob.d_eff
         for i in range(1, n-1):
             cst2 = 2 * prm_sim.dr**2 * prm_sim.mesh[i]  # 2 * r_i * dr^2
-            A[i][i-1] = cst1*(prm_sim.dr - 2*prm_sim.mesh[i])
-            A[i][i] = cst2 + 4*cst1*prm_sim.mesh[i]
-            A[i][i+1] = -cst1*(prm_sim.dr + 2*prm_sim.mesh[i])
-            B[i] = cst2*(c[i] - prm_sim.dt*prm_prob.s)
-
-        # Dirichlet en r = r
-        A[-1][-1] = 1
-        B[-1] = prm_prob.ce
+            a[i][i-1] = cst1*(prm_sim.dr - 2*prm_sim.mesh[i])
+            a[i][i] = cst2 + 4*cst1*prm_sim.mesh[i]
+            a[i][i+1] = -cst1*(prm_sim.dr + 2*prm_sim.mesh[i])
+            b[i] = cst2*(c[i] - prm_sim.dt*prm_prob.s)
 
         # Resolution du systeme lineaire
-        c = np.linalg.solve(A, B)
+        c = np.linalg.solve(a, b)
         tf += prm_sim.dt
         diff = abs(sum(c)-sum_c_prec)/abs(sum_c_prec)
-    return tf, c
+    prm_sim.c = c
+    prm_sim.tf = tf
+
+
+#%% appliquer_conditions_frontieres
+
+def appliquer_conditions_frontieres(a, b, dirichlet):
+    """
+    Fonction qui ajoute les conditions frontieres dans le syteme lineaire
+        - En r = 0 : Un schema Gear avant est utilise pour approximer le gradient
+                     de concentration (ordre 2) et imposer une condition de symetrie
+        - En r = R : Une condition de Dirichlet est imposee
+
+    Entrees :
+        - a : array n x n - Matrice des coefficients du syteme lineaire
+        - b : array n - Vecteur membre de droite du systeme lineaire
+        - dirichlet : float - Condition de Dirichlet imposee en r = R
+
+    Sortie : aucune
+    """
+    # Gear avant en r = 0
+    a[0][0] = 3
+    a[0][1] = -4
+    a[0][2] = 1
+    b[0] = 0
+
+    # Dirichlet en r = r
+    a[-1][-1] = 1
+    b[-1] = dirichlet
 
 
 #%% analytique
-"""
-Fonction qui calcule la solution analytique aux points du maillage.
-
-Entrees :
-    - prm_prob : Objet qui contient les parametres du probleme
-        - c0 : Concentrations initiales [mol/m^3]
-        - ce : Concentration de sel de l'eau salee [mol/m^3]
-        - d_eff : Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-        - r : Rayon du pilier cylindrique [m^3]
-        - s : Terme source constant (reaction d'ordre 0) [mol/m^3/s]
-    - mesh : Vecteur conteant les noeuds (r_i) du probleme 1D [m]
-    
-Sortie :
-    - c : Le profile de concentration radial analytique au régime permanent [mol/m^3]
-"""
 def analytique(prm_prob, mesh):
-    c = [0.25*prm_prob.s/prm_prob.d_eff * prm_prob.r**2 * (r**2/prm_prob.r**2 - 1) + prm_prob.ce for r in mesh]
+    """
+    Fonction qui calcule la solution analytique aux points du maillage.
+
+    Entrees :
+        - prm_prob : Objet qui contient les parametres du probleme
+            - c0 : float - Concentrations initiales [mol/m^3]
+            - ce : float - Concentration de sel de l'eau salee [mol/m^3]
+            - r : float - Rayon du pilier cylindrique [m]
+            - d_eff : float - Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+            - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source (0 ou 1) []
+            - s : float - Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+            - k : float - Constante de réaction pour la reaction d'ordre 1 [s^{-1}]
+        - mesh : array de float - Vecteur conteant les noeuds (r_i) du probleme 1D [m]
+
+    Sortie :
+        - c : array de float - Le profile de concentration radial analytique au regime
+                               permanent [mol/m^3]
+    """
+    c = [0.25*prm_prob.s/prm_prob.d_eff * prm_prob.r**2 * (r**2/prm_prob.r**2 - 1)
+         + prm_prob.ce for r in mesh]
     return c
 
 
 #%% erreur_l1
-"""
-Fonction qui calcule la valeur de l'erreur L1 de la solution numerique obtenue
-
-Entree :
-    - c_num : Solution numerique du probleme 1D [mol/m^3]
-    - c_analytique : Solution analytique du probleme 1D [mol/m^3]
-
-Sortie :
-    - erreur : Norme de l'erreur L1 de la solution numerique [mol/m^3]
-"""
 def erreur_l1(c_num, c_analytique):
-    erreur = sum([abs(ci_num - ci_analytique) for ci_num, ci_analytique in zip(c_num, c_analytique)])
+    """
+    Fonction qui calcule la valeur de l'erreur L1 de la solution numerique obtenue
+
+    Entree :
+        - c_num : Solution numerique du probleme 1D [mol/m^3]
+        - c_analytique : Solution analytique du probleme 1D [mol/m^3]
+
+    Sortie :
+        - erreur : Norme de l'erreur L1 de la solution numerique [mol/m^3]
+    """
+    erreur = sum(abs(ci_num - ci_analytique)
+                  for ci_num, ci_analytique in zip(c_num, c_analytique))
     erreur *= 1/len(c_num)
     return erreur
 
 
 #%% erreur_l2
-"""
-Fonction qui calcule la valeur de l'erreur L2 de la solution numerique obtenue
-
-Entree :
-    - c_num : Solution numerique du probleme 1D [mol/m^3]
-    - c_analytique : Solution analytique du probleme 1D [mol/m^3]
-
-Sortie :
-    - erreur : Norme de l'erreur L2 de la solution numerique [mol/m^3]
-"""
 def erreur_l2(c_num, c_analytique):
-    erreur = sum([abs(ci_num - ci_analytique)**2 for ci_num, ci_analytique in zip(c_num, c_analytique)])
+    """
+    Fonction qui calcule la valeur de l'erreur L2 de la solution numerique obtenue
+
+    Entree :
+        - c_num : Solution numerique du probleme 1D [mol/m^3]
+        - c_analytique : Solution analytique du probleme 1D [mol/m^3]
+
+    Sortie :
+        - erreur : Norme de l'erreur L2 de la solution numerique [mol/m^3]
+    """
+    erreur = sum(abs(ci_num - ci_analytique)**2
+                  for ci_num, ci_analytique in zip(c_num, c_analytique))
     erreur *= 1/len(c_num)
     erreur = np.sqrt(erreur)
     return erreur
 
 
 #%% erreur_linfty
-"""
-Fonction qui calcule la valeur de l'erreur L_\infty de la solution numerique obtenue
-
-Entree :
-    - c_num : Solution numerique du probleme 1D [mol/m^3]
-    - c_analytique : Solution analytique du probleme 1D [mol/m^3]
-
-Sortie :
-    - erreur : Norme de l'erreur L_\infty de la solution numerique [mol/m^3]
-"""
 def erreur_linfty(c_num, c_analytique):
-    erreur = max([abs(ci_num - ci_analytique) for ci_num, ci_analytique in zip(c_num, c_analytique)])
+    """
+    Fonction qui calcule la valeur de l'erreur L_infty de la solution numerique obtenue
+
+    Entree :
+        - c_num : Solution numerique du probleme 1D [mol/m^3]
+        - c_analytique : Solution analytique du probleme 1D [mol/m^3]
+
+    Sortie :
+        - erreur : Norme de l'erreur L_infty de la solution numerique [mol/m^3]
+    """
+    erreur = max(abs(ci_num - ci_analytique)
+                  for ci_num, ci_analytique in zip(c_num, c_analytique))
     return erreur
--- code_amishga/devoir1_main.py
+++ code_amishga/devoir1_main.py
@@ -10,34 +10,35 @@ Date de creation du fichier : 5 fevrier 2024
 
 #%% Importation des modules
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-# import sys args
 
 # Importation des fonctions
 try:
-    from devoir1_functions import *
-except:
+    from devoir1_functions import (mdf1_rxn_0, mdf2_rxn_0, analytique, erreur_l1,
+                                   erreur_l2, erreur_linfty)
+except ImportError:
     print("ERREUR ! Il y a une erreur fatale dans le fichier devoir1_functions.py")
 
 #%% Donnees du probleme et definition des classes
 
-"""
-Classe qui contient les differents parametres du probleme selon le type de terme source
-
-Entree:
-    - ordre_de_rxn : Ordre de la reaction (0 ou 1) []
-    
-Attributs:
-    - c0 : float - Concentrations initiales [mol/m^3]
-    - ce : float - Concentration de sel de l'eau salee [mol/m^3]
-    - r : float - Rayon du pilier cylindrique [m]
-    - d_eff : float - Coefficient de diffusion effectif de sel dans le beton [m^2/s]
-    - ordre_de_rxn : int - Ordre de la reaction (0 ou 1) []
-    - s : float - Terme source constant (reaction d'ordre 0) [mol/m^3/s]
-    - k : float - Constante de réaction pour la reaction d'ordre 1 [s^{-1}]
-"""
 class ParametresProb:
-    def __init__(self, ordre_de_rxn=0):
+    """
+    Classe qui contient les differents parametres du probleme selon le type de terme source
+
+    Entree:
+        - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source (0 ou 1) []
+
+    Attributs:
+        - c0 : float - Concentrations initiales [mol/m^3]
+        - ce : float - Concentration de sel de l'eau salee [mol/m^3]
+        - r : float - Rayon du pilier cylindrique [m]
+        - d_eff : float - Coefficient de diffusion effectif de sel dans le beton [m^2/s]
+        - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source (0 ou 1) []
+        - s : float - Terme source constant (reaction d'ordre 0) [mol/m^3/s]
+        - k : float - Constante de réaction pour la reaction d'ordre 1 [s^{-1}]
+    """
+    def __init__(self, ordre_de_rxn: int):
         self.c0 = 0.0
         self.ce = 12.0
         self.r = 1*0.5
@@ -47,69 +48,74 @@ class ParametresProb:
             self.s = 8e-9
         elif self.ordre_de_rxn == 1:
             self.k = 4e-9
-            raise Exception("Cet ordre n'est pas encore implememte.")
+            raise ValueError("Cet ordre n'est pas encore implememte.")
         else:
-            raise Exception("L'ordre de reaction doit etre de 0 ou 1.")
+            raise ValueError("L'ordre de reaction doit etre de 0 ou 1.")
 
-"""
-Classe qui contient les parametres de simulation pour une simulation donnee
 
-Entrees:
-    - prm_rxn : Object contenant les donnees du probleme
-        - r : Rayon du pilier cylindrique [m^3]
-    - p_n_noeuds : Nombre de noeuds dans le maillage [noeud]
-    - p_mdf : 
-   
-Attributs :
-    - n_noeuds : int - Nombre de noeuds dans le maillage [noeud]
-    - dr : float - Pas en espace des differents maillages [m]
-    - dt : float - Pas de temps des differents maillages [s]
-    - mesh : array of floats - Vecteur conteant les noeuds (r_i) du probleme 1D [m]
-    - tol : float - Tolerance relative pour l'atteinte du regime permanent []
-    - c : array of floats - Solution une fois l'atteinte du regime permanent [mol/m^3]
-    - tf : float - Temps de fin de la simulation [s]
-    - mdf : 
-    - ordre_de_rxn : 
-"""
 class ParametresSim:
-    def __init__(self, prm_rxn, p_n_noeuds, p_mdf):
+    """
+    Classe qui contient les parametres de simulation pour une simulation donnee
+
+    Entrees:
+        - prm_rxn : Objet contenant les donnees du probleme
+            - r : Rayon du pilier cylindrique [m^3]
+            - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source (0 ou 1) []
+        - p_n_noeuds : int - Nombre de noeuds dans le maillage [noeud]
+        - p_mdf : int - Ordre global en espace de la methode des differences finies utilisee []
+
+    Attributs :
+        - n_noeuds : int - Nombre de noeuds dans le maillage [noeud]
+        - dr : float - Pas en espace des differents maillages [m]
+        - dt : float - Pas de temps des differents maillages [s]
+        - mesh : array of floats - Vecteur conteant les noeuds (r_i) du probleme 1D [m]
+        - tol : float - Tolerance relative pour l'atteinte du regime permanent []
+        - c : array of floats - Solution une fois l'atteinte du regime permanent [mol/m^3]
+        - tf : float - Temps de fin de la simulation [s]
+        - mdf : int - Ordre global en espace de la methode des differences finies utilisee []
+        - ordre_de_rxn : int - Ordre de la cinetique de reaction du terme source []
+    """
+    def __init__(self, prm_rxn: ParametresProb, p_n_noeuds: int, p_mdf: int):
         self.n_noeuds = p_n_noeuds
         self.dr = prm_rxn.r/(self.n_noeuds-1)
         self.dt = 0.5*self.dr**2/prm_rxn.d_eff
-        self.mesh = np.linspace(0,prm_rxn.r,self.n_noeuds)
+        self.mesh = np.linspace(0, prm_rxn.r, self.n_noeuds)
         self.tol = 1e-8
         self.c = np.zeros(self.n_noeuds)
         self.tf = 0
         self.mdf = p_mdf
-        self.ordre_de_rxn = 0
+        self.ordre_de_rxn = prm_rxn.ordre_de_rxn
 
 
 #%% Initialisation des objects contenants les parametres du probleme pour une reaction d'ordre 0
 #   et 1
 
 prm_rxn_0 = ParametresProb(ordre_de_rxn=0)
-# prm_rxn_1 = ParametresProb(ordre_de_rxn=1) # pas encore implemente
+# prm_rxn_1 = ParametresProb(ordre_de_rxn=1)  # pas encore implemente
 
 
 #%% Discretisation pour la reaction d'ordre 0
 
-n_noeuds = [10, 20, 40, 80, 160] # Liste de nombre de noeuds pour les differents maillages [noeud]
+n_noeuds_liste = [10, 20, 40, 80]   # Liste de nombre de noeuds pour les differents
+                                    # maillages [noeud]
 
 # Initialisation des differents maillages a l'etude
 prm_simulations_mdf1_rxn0 = []
 prm_simulations_mdf2_rxn0 = []
-for i in range(len(n_noeuds)):
-    prm_simulations_mdf1_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds[i], 1))
-    prm_simulations_mdf2_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds[i], 2))
+for i, n_noeuds in enumerate(n_noeuds_liste):
+    prm_simulations_mdf1_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds, 1))
+    prm_simulations_mdf2_rxn0.append(ParametresSim(prm_rxn_0, n_noeuds, 2))
 
 
 #%% Resolution du probleme
 
 for prm_simulation in [prm_simulations_mdf1_rxn0, prm_simulations_mdf2_rxn0]:
+    print("****************************************************************************")
     for prm_sim in prm_simulation:
         mdf_i = prm_sim.mdf
         # Calcul de la concentration au regime permanent
-        exec(f"prm_sim.tf,prm_sim.c = mdf{mdf_i}_rxn_0(prm_rxn_0, prm_sim)")
+        # pylint: disable-next=exec-used
+        exec(f"mdf{mdf_i}_rxn_0(prm_rxn_0, prm_sim)")
 
         # Exportation des solutions dans des fichiers csv
         exported_data = pd.DataFrame({'r': prm_sim.mesh, 'C(r)': prm_sim.c})
@@ -118,11 +124,13 @@ for prm_simulation in [prm_simulations_mdf1_rxn0, prm_simulations_mdf2_rxn0]:
 
         # Affichage au terminal
         print("****************************************************************************")
+        print(f"Ordre de la methode des differences finies : {mdf_i}")
+        print(f"Ordre de la reaction du terme source : {prm_sim.ordre_de_rxn}")
         print(f"Nombre de noeuds : {prm_sim.n_noeuds} noeuds")
         print(f"dr = {prm_sim.dr} m")
         print(f"dt = {prm_sim.dt} s")
         print(f"tf = {prm_sim.tf} s")
-        print("****************************************************************************")
+print("****************************************************************************")
 
 #%% Etude de l'erreur
 
